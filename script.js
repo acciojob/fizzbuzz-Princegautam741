5e69560c70106d56ae010d8e7faefc1f0629d1fb
prince gautam <55788541+Princegautam741@users.noreply.github.com>
2023-12-11 17:38:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-//your JS code here. If required.
+// Write a program that prints the numbers from 1 to 20 using alert.
+
+let result = "";
+
+for (let i = 1; i <= 20; i++) {
+    // Check for multiples of both three and five
+    if (i % 3 === 0 && i % 5 === 0) {
+        result += "FizzBuzz\n";
+    }
+    // Check for multiples of three
+    else if (i % 3 === 0) {
+        result += "Fizz\n";
+    }
+    // Check for multiples of five
+    else if (i % 5 === 0) {
+        result += "Buzz\n";
+    }
+    // Append the number itself as a string
+    else {
+        result += i.toString() + "\n";
+    }
+}
+
+// Display the result in a single alert
+alert(result);
